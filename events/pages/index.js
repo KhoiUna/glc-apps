5acb27861e66fe39bd091e3b874909a4e3fd6b8e
KhoiNguyen <thorwaitson@gmail.com>
2021-11-08 16:14:39 -0600

    events: udpate /events folder

--- events/pages/index.js
+++ events/pages/index.js
@@ -1,11 +1,21 @@
+import Head from "next/head";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [eventName, setEventName] = useState("");
+  useEffect(() => {
+    const url = new URL(window.location);
+    const eventName = url.searchParams.get("eventName");
+    setEventName(eventName);
+  }, []);
+
   return (
     <>
       <Head>
         <title>GLC Events</title>
       </Head>
 
-      <main>hi</main>
+      <main>Event name: {eventName}</main>
     </>
   );
 }
