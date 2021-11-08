3c66ecfe372151bca748ebbd07c2c52df88b5ba7
KhoiNguyen <thorwaitson@gmail.com>
2021-11-08 11:02:42 -0600

    admin: add events.js page

---
+++ admin/pages/events.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import Layout from "../containers/layout";
+import { origin } from "../config/config";
+
+export default function Events({}) {
+  return (
+    <Layout componentName="Events">
+      <p>hi</p>
+    </Layout>
+  );
+}
