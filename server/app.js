b2613bce1c735121eb123e479e0fa610766e783f
KhoiNguyen <thorwaitson@gmail.com>
2021-11-08 22:12:38 -0600

    server: add socket

--- server/app.js
+++ server/app.js
@@ -5,6 +5,14 @@ const app = express();
 const helmet = require("helmet");
 const compression = require("compression");
 const { origin } = require("./config");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 const PORT = process.env.PORT || 5000;
 
@@ -38,12 +46,32 @@ app.use("/api/cancel", cancelRoute);
 const timeRoute = require("./routes/time-route");
 app.use("/api/time", timeRoute);
 
+//Socket
+io.on("connection", (socket) => {
+  console.log("------User connected------");
+
+  socket.on("submit", ({ firstName, lastName, lNumber }) => {
+    console.log({ firstName, lastName, lNumber });
+    io.emit("submit", {
+      firstName,
+      lastName,
+      lNumber,
+      submitted_date: new Date().toUTCString(),
+    });
+  });
+
+  //Listen when users disconnect
+  socket.on("disconnect", () => {
+    console.log("------User disconnected------");
+  });
+});
+
 //Error handling
 app.use((err, req, res, next) => {
   const status = err.status || 500;
   res.status(status).send(err.message);
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("On port " + PORT);
 });
