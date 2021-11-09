9a89496c5afd94fa42ca612c33e186987b36d800
KhoiNguyen <thorwaitson@gmail.com>
2021-11-08 23:36:15 -0600

    admin: add submissions_tab.js to comp

--- admin/components/events/submissions_tab.js
+++ admin/components/events/submissions_tab.js
@@ -1,7 +1,42 @@
+import io from "socket.io-client";
+import { origin } from "../../config/config";
+import Paper from "@mui/material/Paper";
+import { useEffect, useState } from "react";
+import { Typography } from "@mui/material";
+
+let socket;
 export default function SubmissionsTab({}) {
-  return (
-    <>
-      <p>hi</p>
-    </>
+  const [submissions, setSubmissions] = useState([]);
+  useEffect(() => {
+    socket = io(origin, {
+      withCredentials: true,
+    });
+
+    socket.on("submit", (submission) => {
+      setSubmissions((prev) => [submission, ...prev]);
+    });
+
+    return () => {
+      socket.removeAllListeners();
+    };
+  }, []);
+
+  return submissions.map(
+    ({ eventName, firstName, lastName, lNumber, submitted_date }, index) => (
+      <Paper
+        key={index}
+        elevation={3}
+        sx={{
+          padding: "0.5rem",
+          margin: "1.25rem 0.7rem",
+        }}
+      >
+        <Typography>
+          <b>Event name:</b> {eventName}
+        </Typography>
+        <b>{firstName + " " + lastName}</b> - L#{lNumber} -{" "}
+        <i>{new Date(submitted_date).toLocaleString()}</i>
+      </Paper>
+    )
   );
 }
