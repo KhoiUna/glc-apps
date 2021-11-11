33a6d2d1238f6b843c773d55ee3611bd0305c071
KhoiNguyen <thorwaitson@gmail.com>
2021-11-11 11:54:33 -0600

    events: add upload img to form

--- admin/components/events/submissions_tab.js
+++ admin/components/events/submissions_tab.js
@@ -3,6 +3,7 @@ import { origin } from "../../config/config";
 import Paper from "@mui/material/Paper";
 import { useEffect, useState } from "react";
 import { Typography } from "@mui/material";
+import Image from "next/image";
 
 let socket;
 export default function SubmissionsTab({}) {
@@ -22,21 +23,45 @@ export default function SubmissionsTab({}) {
   }, []);
 
   return submissions.map(
-    ({ eventName, firstName, lastName, lNumber, submitted_date }, index) => (
-      <Paper
-        key={index}
-        elevation={3}
-        sx={{
-          padding: "0.5rem",
-          margin: "1.25rem 0.7rem",
-        }}
-      >
-        <Typography>
-          <b>Event name:</b> {eventName}
-        </Typography>
-        <b>{firstName + " " + lastName}</b> - L#{lNumber} -{" "}
-        <i>{new Date(submitted_date).toLocaleString()}</i>
-      </Paper>
-    )
+    (
+      { eventName, firstName, lastName, lNumber, imgBlob, submitted_date },
+      index
+    ) => {
+      function blobToBase64(imgBlob) {
+        const { blob, type } = imgBlob;
+
+        const TYPED_ARRAY = new Uint8Array(blob);
+        const STRING_CHAR = TYPED_ARRAY.reduce((data, byte) => {
+          return data + String.fromCharCode(byte);
+        }, "");
+        const base64String = type + "," + btoa(STRING_CHAR);
+
+        return base64String;
+      }
+
+      return (
+        <Paper
+          key={index}
+          elevation={3}
+          sx={{
+            padding: "0.5rem",
+            margin: "1.25rem 0.7rem",
+          }}
+        >
+          <Typography>
+            <b>Event name:</b> {eventName}
+          </Typography>
+          <b>{firstName + " " + lastName}</b> - L#{lNumber} -{" "}
+          <i>{new Date(submitted_date).toLocaleString()}</i>
+          <Image
+            src={blobToBase64(imgBlob)}
+            height="200"
+            alt={`${firstName + " " + lastName}'s submission image'`}
+            width={150}
+            height={150}
+          />
+        </Paper>
+      );
+    }
   );
 }
--- events/pages/index.js
+++ events/pages/index.js
@@ -6,6 +6,7 @@ import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import io from "socket.io-client";
 import { origin } from "../config/config";
+import Image from "next/image";
 
 let socket;
 export default function Home() {
@@ -38,8 +39,48 @@ export default function Home() {
     }));
   };
 
-  const handleClick = () => {
-    socket.emit("submit", { eventName, ...formValue });
+  const [imgSrc, setImgSrc] = useState("");
+  const [imgBlob, setImgBlob] = useState({});
+
+  const uploadImage = () => {
+    const file = document.querySelector("input[type=file]").files[0];
+    const reader = new FileReader();
+
+    reader.addEventListener(
+      "load",
+      () => {
+        const base64String = reader.result;
+        fetch(base64String)
+          .then((res) => res.blob())
+          .then((blob) => {
+            setImgBlob({ blob, type: base64String.split(",")[0] });
+          });
+
+        setImgSrc(reader.result);
+      },
+      false
+    );
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      console.log(imgBlob);
+      socket.emit("submit", { eventName, ...formValue, imgBlob });
+
+      setFormValue({
+        firstName: "",
+        lastName: "",
+        lNumber: "",
+      });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -61,7 +102,7 @@ export default function Home() {
           <b>Event name:</b> {eventName}
         </Typography>
 
-        <form onChange={handleChange}>
+        <form onChange={handleChange} onSubmit={handleSubmit}>
           <Stack
             sx={{ margin: "1.5rem 1rem" }}
             direction="row"
@@ -119,7 +160,31 @@ export default function Home() {
             />
           </Stack>
 
-          <Button variant="contained" onClick={handleClick}>
+          <Stack
+            sx={{ margin: "1.5rem 1rem" }}
+            direction="row"
+            justifyContent="space-evenly"
+            alignItems="center"
+            spacing={2}
+          >
+            <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+              Upload image:
+            </Typography>
+            <input type="file" onChange={uploadImage} required />
+          </Stack>
+          {imgSrc && (
+            <div style={{ margin: "1rem" }}>
+              <Image
+                src={imgSrc}
+                height="200"
+                alt="Image preview..."
+                width={150}
+                height={150}
+              />
+            </div>
+          )}
+
+          <Button variant="contained" type="submit">
             Submit
           </Button>
         </form>
