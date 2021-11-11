08130a2fd35f512441cc7d5cea5a0b7c4f406aef
KhoiNguyen <thorwaitson@gmail.com>
2021-11-11 14:39:39 -0600

    admin: add EventsViewDialog

---
+++ admin/components/events/events_view_dialog.js
@@ -0,0 +1,54 @@
+import Dialog from "@mui/material/Dialog";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Typography from "@mui/material/Typography";
+import { appBarTheme } from "../../themes/themes";
+
+export default function EventsViewDialog({
+  toggleOpenEventView,
+  openEventView,
+  eventName,
+}) {
+  console.log(eventName);
+
+  return (
+    <Dialog fullScreen open={openEventView} onClose={toggleOpenEventView}>
+      <AppBar sx={{ ...appBarTheme, position: "relative" }}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            onClick={toggleOpenEventView}
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>
+          <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+            View event
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <div style={{ textAlign: "center", marginTop: "4rem" }}>
+        <img
+          width={150}
+          src={`https://api.qrserver.com/v1/create-qr-code/?data=https://glc-events.vercel.app?eventName=${eventName}`}
+        />
+      </div>
+
+      <div style={{ textAlign: "center", margin: "1rem auto" }}>
+        <a
+          href={`https://glc-events.vercel.app?eventName=${eventName}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Typography variant="h6" component="div">
+            {`https://glc-events.vercel.app?eventName=${eventName}`}
+          </Typography>
+        </a>
+      </div>
+    </Dialog>
+  );
+}
