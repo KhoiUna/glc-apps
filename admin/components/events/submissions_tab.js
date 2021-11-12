081ff21f16e6586d5defb7670ea6abfa4964808a
KhoiNguyen <thorwaitson@gmail.com>
2021-11-12 12:51:37 -0600

    admin: update events' SubmissionsTab comp

--- admin/components/events/submissions_tab.js
+++ admin/components/events/submissions_tab.js
@@ -21,48 +21,50 @@ export default function SubmissionsTab({}) {
     };
   }, []);
 
-  const blobToBase64 = (imgBlob) => {
-    const { blob, type } = imgBlob;
-
-    const TYPED_ARRAY = new Uint8Array(blob);
-    const STRING_CHAR = TYPED_ARRAY.reduce((data, byte) => {
-      return data + String.fromCharCode(byte);
-    }, "");
-    const base64String = type + "," + btoa(STRING_CHAR);
-    console.log(base64String);
-    return base64String;
-  };
-
   return submissions.map(
     (
       { eventName, firstName, lastName, lNumber, imgBlob, submitted_date },
       index
-    ) => (
-      <Paper
-        key={index}
-        elevation={3}
-        sx={{
-          padding: "0.5rem",
-          margin: "1.25rem 0.7rem",
-        }}
-      >
-        <Typography>
-          <b>Event name:</b> {eventName}
-        </Typography>
-        <b>{firstName + " " + lastName}</b> - L#{lNumber} -{" "}
-        <i>{new Date(submitted_date).toLocaleString()}</i>
-        <div
-          style={{
-            margin: "1.5rem auto",
-            textAlign: "center",
+    ) => {
+      const blobToBase64 = (imgBlob) => {
+        const { blob, type } = imgBlob;
+
+        const TYPED_ARRAY = new Uint8Array(blob);
+        const STRING_CHAR = TYPED_ARRAY.reduce((data, byte) => {
+          return data + String.fromCharCode(byte);
+        }, "");
+
+        const base64String = type + "," + btoa(STRING_CHAR);
+        return base64String;
+      };
+
+      return (
+        <Paper
+          key={index}
+          elevation={3}
+          sx={{
+            padding: "0.5rem",
+            margin: "1.25rem 0.7rem",
           }}
         >
-          <img
-            src={blobToBase64(imgBlob)}
-            alt={`${firstName + " " + lastName}'s submission image'`}
-          />
-        </div>
-      </Paper>
-    )
+          <Typography>
+            <b>Event name:</b> {eventName}
+          </Typography>
+          <b>{firstName + " " + lastName}</b> - L#{lNumber} -{" "}
+          <i>{new Date(submitted_date).toLocaleString()}</i>
+          <div
+            style={{
+              margin: "1.5rem auto",
+              textAlign: "center",
+            }}
+          >
+            <img
+              src={blobToBase64(imgBlob)}
+              alt={`${firstName + " " + lastName}'s submission image'`}
+            />
+          </div>
+        </Paper>
+      );
+    }
   );
 }
