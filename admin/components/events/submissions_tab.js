bce3741223f6831ce576d253cbf106c78c402a83
KhoiNguyen <thorwaitson@gmail.com>
2021-11-23 04:08:33 -0600

    admin: store submissions IndexedDB-SubmissionsTab

--- admin/components/events/submissions_tab.js
+++ admin/components/events/submissions_tab.js
@@ -3,23 +3,91 @@ import { origin } from "../../config/config";
 import Paper from "@mui/material/Paper";
 import { useEffect, useState } from "react";
 import { Typography } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import CancelIcon from "@mui/icons-material/Cancel";
+import CheckIcon from "@mui/icons-material/Check";
 
 let socket;
 export default function SubmissionsTab({}) {
   const [submissions, setSubmissions] = useState([]);
+  useEffect(() => {
+    if (!window.indexedDB)
+      console.log(
+        "Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available."
+      );
+
+    const openRequest = indexedDB.open("submissionsIndexedDb", 3);
+    openRequest.onerror = (event) => {
+      console.error("IndexedDB not supported");
+    };
+    openRequest.onupgradeneeded = (event) => {
+      const db = event.target.result;
+
+      // Create submissionObjStore object store
+      const submissionObjStore = db.createObjectStore("submissions", {
+        autoIncrement: true,
+      });
+    };
+    openRequest.onsuccess = (event) => {
+      const db = event.target.result;
+      db.onversionchange = () => {
+        db.close();
+        alert("Database is outdated, please reload the page.");
+      };
+
+      const submissionsObjStore = db
+        .transaction("submissions")
+        .objectStore("submissions");
+
+      const request = submissionsObjStore.getAll();
+      request.onsuccess = (event) => {
+        setSubmissions(event.target.result.reverse());
+      };
+
+      request.onerror = () => {
+        console.log("Error", request.error);
+      };
+    };
+  }, []);
+
   useEffect(() => {
     socket = io(origin, {
       withCredentials: true,
     });
 
     socket.on("submit", (submission) => {
-      setSubmissions((prev) => [submission, ...prev]);
+      const openRequest = indexedDB.open("submissionsIndexedDb", 3);
+
+      openRequest.onerror = function (event) {
+        console.error("IndexedDB not supported");
+      };
+      openRequest.onsuccess = (event) => {
+        const db = event.target.result;
+        db.onversionchange = () => {
+          db.close();
+          alert("Database is outdated, please reload the page.");
+        };
+
+        const submissionsObjStore = db
+          .transaction("submissions", "readwrite")
+          .objectStore("submissions");
+
+        submissionsObjStore.add(submission);
+
+        const request = submissionsObjStore.getAll();
+        request.onsuccess = (event) => {
+          setSubmissions((prev) => [submission, ...prev]);
+        };
+        request.onerror = () => {
+          console.log("Error", request.error);
+        };
+      };
     });
 
     return () => {
       socket.removeAllListeners();
     };
-  }, []);
+  }, [submissions]);
 
   return submissions.map(
     (
@@ -50,18 +118,39 @@ export default function SubmissionsTab({}) {
           <Typography>
             <b>Event name:</b> {eventName}
           </Typography>
-          <b>{firstName + " " + lastName}</b> - L#{lNumber} -{" "}
-          <i>{new Date(submitted_date).toLocaleString()}</i>
+          <Typography>
+            <b>From:</b> {firstName + " " + lastName}
+          </Typography>
+          <Typography>
+            <b>Submitted on:</b>{" "}
+            <i>{new Date(submitted_date).toLocaleString()}</i>
+          </Typography>
+
           <div
             style={{
               margin: "1.5rem auto",
-              textAlign: "center",
             }}
           >
             <img
+              width="220rem"
               src={blobToBase64(imgBlob)}
-              alt={`${firstName + " " + lastName}'s submission image'`}
+              alt={`${firstName + " " + lastName}'s submission image`}
             />
+          </div>
+
+          <div
+            style={{
+              margin: "1.5rem auto",
+              textAlign: "right",
+            }}
+          >
+            <IconButton aria-label="delete">
+              <CancelIcon />
+            </IconButton>
+
+            <IconButton aria-label="delete">
+              <CheckIcon />
+            </IconButton>
           </div>
         </Paper>
       );
