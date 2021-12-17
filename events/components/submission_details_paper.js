549b4cadcf03468d0000931a217b43da3371e89b
KhoiNguyen <thorwaitson@gmail.com>
2021-12-17 14:13:41 -0600

    events: update submission_details_paper.js

--- events/components/submission_details_paper.js
+++ events/components/submission_details_paper.js
@@ -5,16 +5,48 @@ import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import { origin } from "../config/config";
+import ProgressBar from "./progress_bar";
+import { useState } from "react";
+import SubmissionUtil from "../utils/SubmissionUtil";
+import Image from "next/image";
+import imageLoader from "../helpers/imageLoader";
 
 export default function SubmissionDetailsPaper({ createSubmissionDetails }) {
+  const [showProgress, setShowProgress] = useState(false);
+  const [progressValue, setProgressValue] = useState(0);
+  const showProgressBar = (status) => {
+    if (status !== "success" && status !== "error")
+      return setShowProgress(status);
+
+    if (status === "success") {
+      setProgressValue(100);
+      return setTimeout(() => {
+        showProgressBar(false);
+        setProgressValue(0);
+      }, 1000);
+    }
+    if (status === "error") {
+      setProgressValue(50);
+    }
+    return setTimeout(() => {
+      showProgressBar(false);
+      setProgressValue(0);
+    }, 1000);
+  };
+
   const onError = (err) => {
     console.error("Error uploading image");
   };
 
+  const [imageURL, setImageURL] = useState("");
   const onSuccess = async (res) => {
     if (res.fileType === "image") {
       const imagePath = res.filePath;
-      //TODO: fetch post to server
+      console.log(imagePath);
+      if (await SubmissionUtil.uploadImage({ imagePath })) {
+        setImageURL(imagePath);
+        showProgressBar("success");
+      }
     }
   };
 
@@ -33,6 +65,7 @@ export default function SubmissionDetailsPaper({ createSubmissionDetails }) {
         <TextField name="eventName" label="Event name" variant="filled" />
       </Stack>
 
+      {showProgress && <ProgressBar progressValue={progressValue} />}
       <Stack
         sx={{ margin: "1.5rem 1rem" }}
         direction="row"
@@ -40,23 +73,40 @@ export default function SubmissionDetailsPaper({ createSubmissionDetails }) {
         alignItems="center"
         spacing={2}
       >
-        <Typography variant="body1" sx={{ fontWeight: "bold" }}>
-          Upload image:
-        </Typography>
-        <IKContext
-          publicKey={process.env.NEXT_PUBLIC_IMGKIT_PUBLIC_KEY}
-          urlEndpoint={process.env.NEXT_PUBLIC_IMGKIT_IMGKIT_URL_ENDPOINT}
-          authenticationEndpoint={`${origin}/api/event/uploadImage/auth`}
-        >
-          <IKUpload
-            fileName="user_avatar.png"
-            onError={onError}
-            onSuccess={onSuccess}
-            folder={"/glc_upload"}
-            // onChange={() => showProgressBar(true)}
-          />
-        </IKContext>
+        {!showProgress && (
+          <>
+            <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+              Upload image:
+            </Typography>
+            <IKContext
+              publicKey={process.env.NEXT_PUBLIC_IMGKIT_PUBLIC_KEY}
+              urlEndpoint={process.env.NEXT_PUBLIC_IMGKIT_IMGKIT_URL_ENDPOINT}
+              authenticationEndpoint={`${origin}/api/event/uploadImage/auth`}
+            >
+              <IKUpload
+                fileName="user_avatar.png"
+                onError={onError}
+                onSuccess={onSuccess}
+                folder={"/glc_upload"}
+                onChange={() => showProgressBar(true)}
+              />
+            </IKContext>
+          </>
+        )}
       </Stack>
+
+      {imageURL && (
+        <div style={{ textAlign: "center", margin: 0 }}>
+          <Image
+            loader={imageLoader}
+            src={imageURL}
+            priority
+            height={300}
+            width={300}
+            alt="uploaded image"
+          />
+        </div>
+      )}
 
       <div
         style={{
