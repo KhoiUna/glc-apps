ab84114e834c7f3a716d62e2f2a905206848e997
KhoiNguyen <thorwaitson@gmail.com>
2021-12-16 18:44:26 -0600

    server: update config.js

--- server/config.js
+++ server/config.js
@@ -7,4 +7,8 @@ module.exports = {
           "https://glc-events.vercel.app",
         ]
       : /(http:\/\/localhost:300)\d{1}$/,
+  eventSubmissionOrigin:
+    process.env.NODE_ENV === "production"
+      ? "https://glc-events.vercel.app"
+      : "http://localhost:3001",
 };
