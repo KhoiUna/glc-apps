028920e3363393c2a107405d339546c0692d42d8
KhoiNguyen <thorwaitson@gmail.com>
2021-12-17 01:11:42 -0600

    events: update UI

---
+++ events/components/submission_details_paper.js
@@ -0,0 +1,82 @@
+import { IKContext, IKUpload } from "imagekitio-react";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+
+export default function SubmissionDetailsPaper() {
+  const onError = (err) => {
+    console.error("Error uploading image");
+  };
+
+  const onSuccess = async (res) => {
+    if (res.fileType === "image") {
+      const avatarPath = res.filePath;
+      //TODO: fetch post to server
+    }
+  };
+
+  return (
+    <Paper elevation={9} sx={{ margin: "2rem 1rem", padding: "0.5rem" }}>
+      <Stack
+        sx={{ margin: "1.5rem 1rem" }}
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="center"
+        spacing={2}
+      >
+        <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+          Event name:
+        </Typography>
+        <TextField
+          name="eventName"
+          label="Event name"
+          variant="filled"
+          required
+        />
+      </Stack>
+
+      <Stack
+        sx={{ margin: "1.5rem 1rem" }}
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="center"
+        spacing={2}
+      >
+        <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+          Upload image:
+        </Typography>
+        <IKContext
+          publicKey={process.env.NEXT_PUBLIC_IMGKIT_PUBLIC_KEY}
+          urlEndpoint={process.env.NEXT_PUBLIC_IMGKIT_IMGKIT_URL_ENDPOINT}
+          authenticationEndpoint={`${origin}/api/event/uploadImage/auth`}
+        >
+          <IKUpload
+            fileName="user_avatar.png"
+            onError={onError}
+            onSuccess={onSuccess}
+            folder={"/glc_upload"}
+            required
+            // onChange={() => showProgressBar(true)}
+          />
+        </IKContext>
+      </Stack>
+
+      <div
+        style={{
+          textAlign: "center",
+          margin: "1rem 0",
+        }}
+      >
+        <Button
+          // onClick={createEvent}
+          variant="contained"
+          aria-label="Create event"
+        >
+          + Add event
+        </Button>
+      </div>
+    </Paper>
+  );
+}
--- events/pages/index.js
+++ events/pages/index.js
@@ -4,11 +4,9 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import io from "socket.io-client";
-import { origin } from "../config/config";
-import { IKContext, IKUpload } from "imagekitio-react";
 import Layout from "../containers/layout";
-import Paper from "@mui/material/Paper";
-import Fab from "@mui/material/Fab";
+import { origin } from "../config/config";
+import SubmissionDetailsPaper from "../components/submission_details_paper";
 
 let socket;
 export default function Home() {
@@ -50,17 +48,6 @@ export default function Home() {
     }
   };
 
-  const onError = (err) => {
-    console.error("Error uploading image");
-  };
-
-  const onSuccess = async (res) => {
-    if (res.fileType === "image") {
-      const avatarPath = res.filePath;
-      //TODO: fetch post to server
-    }
-  };
-
   return (
     <Layout>
       <div style={{ margin: "1rem 0 1rem 2rem" }}>
@@ -92,66 +79,9 @@ export default function Home() {
           />
         </Stack>
 
-        <Paper elevation={3} sx={{ margin: "1.5rem 1rem", padding: "0.5rem" }}>
-          <Stack
-            sx={{ margin: "1.5rem 1rem" }}
-            direction="row"
-            justifyContent="space-evenly"
-            alignItems="center"
-            spacing={2}
-          >
-            <Typography variant="body1" sx={{ fontWeight: "bold" }}>
-              Event name:
-            </Typography>
-            <TextField
-              name="eventName"
-              label="Event name"
-              variant="filled"
-              required
-            />
-          </Stack>
-
-          <Stack
-            sx={{ margin: "1.5rem 1rem" }}
-            direction="row"
-            justifyContent="space-evenly"
-            alignItems="center"
-            spacing={2}
-          >
-            <Typography variant="body1" sx={{ fontWeight: "bold" }}>
-              Upload image:
-            </Typography>
-            <IKContext
-              publicKey={process.env.NEXT_PUBLIC_IMGKIT_PUBLIC_KEY}
-              urlEndpoint={process.env.NEXT_PUBLIC_IMGKIT_IMGKIT_URL_ENDPOINT}
-              authenticationEndpoint={`${origin}/api/event/uploadImage/auth`}
-            >
-              <IKUpload
-                fileName="user_avatar.png"
-                onError={onError}
-                onSuccess={onSuccess}
-                folder={"/glc_upload"}
-                required
-                // onChange={() => showProgressBar(true)}
-              />
-            </IKContext>
-          </Stack>
-
-          <div
-            style={{
-              textAlign: "center",
-              margin: "1rem 0",
-            }}
-          >
-            <Button
-              // onClick={createEvent}
-              variant="contained"
-              aria-label="Create event"
-            >
-              + Add event
-            </Button>
-          </div>
-        </Paper>
+        {new Array(2).fill({}).map((i) => (
+          <SubmissionDetailsPaper />
+        ))}
 
         <div style={{ textAlign: "center" }}>
           <Button variant="contained" type="submit">
