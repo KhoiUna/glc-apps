4891db8893677f657a20137df7f84ce859c82b8c
KhoiNguyen <thorwaitson@gmail.com>
2021-12-17 14:11:51 -0600

    events: add progress_bar.js

---
+++ events/components/progress_bar.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import LinearProgress from "@mui/material/LinearProgress";
+
+export default function ProgressBar({ progressValue }) {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prev) => {
+        const diff = Math.random() * 10;
+        return Math.min(prev + diff, 100);
+      });
+    }, 300);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div
+      style={{
+        margin: "2rem",
+      }}
+    >
+      <LinearProgress variant="determinate" value={progressValue || progress} />
+    </div>
+  );
+}
