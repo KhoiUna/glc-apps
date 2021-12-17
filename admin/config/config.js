e0c7c1cd51c526b5cd900a61daacdd68caba5966
KhoiNguyen <thorwaitson@gmail.com>
2021-12-16 20:25:27 -0600

    admin: update config.js

--- admin/config/config.js
+++ admin/config/config.js
@@ -2,4 +2,7 @@ module.exports = {
   origin:
     process.env.NEXT_PUBLIC_ORIGIN ||
     "https://cooking-reservation.herokuapp.com",
+  submissionOrigin:
+    process.env.NEXT_PUBLIC_SUBMISSION_ORIGIN ||
+    "https://glc-events.vercel.app",
 };
