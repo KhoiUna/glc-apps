e1381621acbbf746d2229b421f8f29bdbb4832db
KhoiNguyen <thorwaitson@gmail.com>
2021-12-17 23:20:47 -0600

    server: add saveStudent.js

---
+++ server/utils/saveStudent.js
@@ -0,0 +1,23 @@
+const Students = require("../db/Students");
+const sequelize = require("../db/connection");
+
+module.exports = async ({ fullName }) => {
+  try {
+    let studentId;
+    await sequelize.transaction(async (t) => {
+      const res = await Students.create(
+        {
+          full_name: fullName,
+        },
+        { transaction: t }
+      );
+
+      studentId = res.dataValues.id;
+    });
+
+    return studentId;
+  } catch (e) {
+    console.error("Error saving student");
+    return;
+  }
+};
