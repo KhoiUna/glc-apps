d5fb5b0d935b7cc0960b204de297e9eff6055722
KhoiNguyen <thorwaitson@gmail.com>
2021-12-20 02:40:32 -0600

    server: update getSubmissions.js for event route

--- server/utils/getSubmissions.js
+++ server/utils/getSubmissions.js
@@ -1,8 +1,5 @@
-const Submissions = require("../db/Submissions");
-const Students = require("../db/Students");
-const SubmissionDetails = require("../db/SubmissionDetails");
-const { Op } = require("sequelize");
-const Events = require("../db/Events");
+const { QueryTypes } = require("sequelize");
+const connection = require("../db/connection");
 
 module.exports = async ({ dateIndex }) => {
   try {
@@ -12,36 +9,15 @@ module.exports = async ({ dateIndex }) => {
     ).toUTCString();
     const sqlLikeDate = date.slice(0, 16);
 
-    const res = await Submissions.findAll({
-      include: [
-        {
-          model: Events,
-          attributes: ["created_at"],
-          where: {
-            created_at: {
-              [Op.like]: `${sqlLikeDate}%`,
-            },
-          },
-        },
-        {
-          model: Students,
-          attributes: ["full_name"],
-        },
-        {
-          model: SubmissionDetails,
-          attributes: ["event_name", "img_url"],
-        },
-      ],
-      where: {
-        status: "pending",
-      },
-    });
-
-    const submissions = res.map((i) => i.dataValues);
+    const submissions = await connection.query(
+      `WITH events_submissions_students AS (WITH submissions_students AS (SELECT event_id, full_name, submitted_at, submissions.status, submissions.id FROM submissions JOIN students ON submissions.student_id = students.id) SELECT created_at, full_name, submitted_at, submissions_students.status, submissions_students.id FROM submissions_students JOIN events ON event_id = events.id) SELECT created_at, full_name, submitted_at, status, event_name, img_url FROM events_submissions_students JOIN submission_details ON events_submissions_students.id = submission_id WHERE created_at LIKE '${sqlLikeDate}%';`,
+      {
+        type: QueryTypes.SELECT,
+      }
+    );
     return submissions;
   } catch (err) {
     console.error("Error getting submissions");
-    console.error(err);
     return;
   }
 };
