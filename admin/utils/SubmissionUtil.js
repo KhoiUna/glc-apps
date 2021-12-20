477d97f61f77349c9832e2b7aed6b40c809f6e19
KhoiNguyen <thorwaitson@gmail.com>
2021-12-19 19:07:10 -0600

    admin: get submissions by date

--- admin/utils/SubmissionUtil.js
+++ admin/utils/SubmissionUtil.js
@@ -1,10 +1,10 @@
 import { origin } from "../config/config";
 
 export default class SubmissionUtil {
-  static async getSubmissions() {
+  static async getSubmissions({ dateIndex }) {
     try {
       const res = await (
-        await fetch(`${origin}/api/event/submissions/all`)
+        await fetch(`${origin}/api/event/submissions/${dateIndex}`)
       ).json();
 
       return res;
