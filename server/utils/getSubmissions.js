54e8bc4aade88017c17a4d218d1b01abb7d45dc6
KhoiNguyen <thorwaitson@gmail.com>
2021-12-21 16:45:59 -0600

    server: update getSubmissions.js

--- server/utils/getSubmissions.js
+++ server/utils/getSubmissions.js
@@ -1,5 +1,6 @@
-const { QueryTypes } = require("sequelize");
-const connection = require("../db/connection");
+const { Op } = require("sequelize");
+const Students = require("../db/Students");
+const Submissions = require("../db/Submissions");
 
 module.exports = async ({ dateIndex }) => {
   try {
@@ -9,15 +10,24 @@ module.exports = async ({ dateIndex }) => {
     ).toUTCString();
     const sqlLikeDate = date.slice(0, 16);
 
-    const submissions = await connection.query(
-      `WITH events_submissions_students AS (WITH submissions_students AS (SELECT event_id, full_name, submitted_at, submissions.status, submissions.id FROM submissions JOIN students ON submissions.student_id = students.id) SELECT created_at, full_name, submitted_at, submissions_students.status, submissions_students.id FROM submissions_students JOIN events ON event_id = events.id) SELECT created_at, full_name, submitted_at, status, event_name, img_url FROM events_submissions_students JOIN submission_details ON events_submissions_students.id = submission_id WHERE created_at LIKE '${sqlLikeDate}%';`,
-      {
-        type: QueryTypes.SELECT,
-      }
-    );
+    const res = await Submissions.findAll({
+      where: {
+        status: "pending",
+        submitted_at: {
+          [Op.like]: `${sqlLikeDate}%`,
+        },
+      },
+      include: {
+        model: Students,
+        attributes: ["full_name"],
+      },
+    });
+
+    const submissions = res.map((i) => i.dataValues);
     return submissions;
   } catch (err) {
     console.error("Error getting submissions");
+    console.error(err);
     return;
   }
 };
