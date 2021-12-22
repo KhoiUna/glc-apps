0ae98a392fff450cfb23114596c673a1df988d09
KhoiNguyen <thorwaitson@gmail.com>
2021-12-22 17:31:11 -0600

    admin: update SubmissionUtil.js

--- admin/utils/SubmissionUtil.js
+++ admin/utils/SubmissionUtil.js
@@ -3,13 +3,21 @@ import { origin } from "../config/config";
 export default class SubmissionUtil {
   static async getSubmissions({ dateIndex }) {
     try {
+      const timeStamp = new Date().getTime();
+      const date = new Date(
+        timeStamp + 24 * 60 * 60 * 1000 * dateIndex
+      ).toUTCString();
+      const sqlLikeDate = date.slice(0, 16);
+      console.log(sqlLikeDate);
+
       const res = await (
-        await fetch(`${origin}/api/event/submissions/${dateIndex}`)
+        await fetch(`${origin}/api/event/submissions?date=${sqlLikeDate}`)
       ).json();
 
       return res;
     } catch (err) {
       console.error("Error getting submissions");
+      return;
     }
   }
 }
