60d211b3d84970f8a96472d40970a24f1656c9eb
KhoiNguyen <thorwaitson@gmail.com>
2021-12-23 10:00:25 -0600

    server: update students.sql

--- server/sql/students.sql
+++ server/sql/students.sql
@@ -1,7 +1,7 @@
 CREATE TABLE students
 (
     id serial NOT NULL,
-    full_name VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255) NOT NULL UNIQUE,
     signature_count INT NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
 );
