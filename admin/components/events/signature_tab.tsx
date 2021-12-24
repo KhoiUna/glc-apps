1da94c9e19425afb98c71fe827dc4270d7270ce0
KhoiNguyen <thorwaitson@gmail.com>
2021-12-24 17:08:55 -0600

    admin: update SignatureTab to download csv

--- admin/components/events/signature_tab.tsx
+++ admin/components/events/signature_tab.tsx
@@ -4,6 +4,10 @@ import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import StudentUtil from "../../utils/StudentUtil";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import Fab from "@mui/material/Fab";
+import { buttonTheme } from "../../themes/themes";
+import { origin } from "../../config/config";
 
 const StudentPaper = ({ item }) => {
   const [openDialog, setOpenDialog] = useState(false);
@@ -61,6 +65,24 @@ export default function SignatureTab({}) {
       });
   }, []);
 
+  const downloadCsv = async () => {
+    try {
+      const blob = await (await fetch(`${origin}/api/student/csv`)).blob();
+
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "student_signatures.csv";
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+
+      URL.revokeObjectURL(url);
+    } catch (err) {
+      console.error("Error downloading csv file");
+    }
+  };
+
   if (isLoading) return <h2 style={{ textAlign: "center" }}>Loading...</h2>;
   if (students.length === 0)
     return (
@@ -72,6 +94,15 @@ export default function SignatureTab({}) {
       {students.map((item, index) => (
         <StudentPaper key={index} item={item} />
       ))}
+
+      <Fab
+        onClick={downloadCsv}
+        color="primary"
+        aria-label="Download student signature CSV file"
+        sx={{ ...buttonTheme, position: "fixed", bottom: 3, right: 9 }}
+      >
+        <FileDownloadIcon />
+      </Fab>
     </>
   );
 }
