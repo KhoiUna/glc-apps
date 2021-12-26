f80d7889a72ce853a7564746e72085643bb680b7
KhoiNguyen <thorwaitson@gmail.com>
2021-12-26 12:19:55 -0600

    server: update getStudents.js

--- server/utils/getStudents.js
+++ server/utils/getStudents.js
@@ -2,7 +2,9 @@ const Students = require("../db/Students");
 
 module.exports = async () => {
   try {
-    const res = await Students.findAll();
+    const res = await Students.findAll({
+      order: ["full_name"],
+    });
     const students = res.map((i) => i.dataValues);
     return students;
   } catch (e) {
