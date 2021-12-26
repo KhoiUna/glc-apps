8cd4f697c559fd84a2fa6dbf3963ed4accac474d
KhoiNguyen <thorwaitson@gmail.com>
2021-12-26 11:38:59 -0600

    server: update reservations.sql

--- server/sql/reservations.sql
+++ server/sql/reservations.sql
@@ -1,10 +1,10 @@
 CREATE TABLE reservations
 (
     id serial NOT NULL,
-    first_name CHAR(255) NOT NULL,
-    last_name CHAR(255) NOT NULL,
-    number_of_people integer NOT NULL,
-    selected_date date NOT NULL,
-    time_slot integer NOT NULL,
-    PRIMARY KEY (id)
+    student_id INT NOT NULL,
+    number_of_people INT NOT NULL,
+    selected_date DATE NOT NULL,
+    time_slot INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(student_id) REFERENCES students(id)
 );
