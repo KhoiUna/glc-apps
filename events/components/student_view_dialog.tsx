b1c6460cc5bab05ed0b8b57e0f6fd8467d5fa94b
KhoiNguyen <thorwaitson@gmail.com>
2021-12-31 09:22:49 -0600

    events: add page for students

---
+++ events/components/student_view_dialog.tsx
@@ -0,0 +1,110 @@
+import Dialog from "@mui/material/Dialog";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Typography from "@mui/material/Typography";
+import { appBarTheme } from "../themes/themes";
+import { useEffect, useState } from "react";
+import StudentUtil from "../utils/StudentUtil";
+import Paper from "@mui/material/Paper";
+import Image from "next/image";
+import imageLoader from "../helpers/imageLoader";
+
+interface StudentViewDialogProps {
+  toggleOpenDialog: () => any;
+  openDialog: boolean;
+  studentId: number;
+  studentName: string;
+  signatureCount: number;
+}
+export default function StudentViewDialog({
+  toggleOpenDialog,
+  openDialog,
+  studentId,
+  studentName,
+  signatureCount,
+}: StudentViewDialogProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [submissionDetails, setSubmissionDetails] = useState([]);
+  useEffect(() => {
+    setIsLoading(true);
+
+    StudentUtil.fetchSubmissionDetails({ studentId })
+      .then((r) => {
+        setSubmissionDetails(r);
+        setIsLoading(false);
+      })
+      .catch((err) =>
+        console.error("Error getting student's approved submission details")
+      );
+  }, []);
+
+  return (
+    <Dialog fullScreen open={openDialog} onClose={toggleOpenDialog}>
+      <AppBar sx={{ ...appBarTheme, position: "relative" }}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            onClick={toggleOpenDialog}
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>
+          <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+            View student
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <div style={{ textAlign: "center", margin: "1rem 0 0 0" }}>
+        <Typography variant="h4" component="div">
+          {studentName}
+        </Typography>
+
+        <Typography variant="h6" component="div">
+          Signature count: {signatureCount} / 8
+        </Typography>
+      </div>
+
+      {isLoading && <h2 style={{ margin: "1rem auto" }}>Loading...</h2>}
+      {!isLoading &&
+        submissionDetails.map((item, index) => (
+          <Paper
+            elevation={5}
+            sx={{ margin: "1rem", padding: "1rem" }}
+            key={index}
+          >
+            <Typography>
+              <b>Event name:</b> {item.event_name}
+            </Typography>
+            <Typography>
+              <b>Date submitted:</b>{" "}
+              {new Date(item.submitted_at).toLocaleDateString()}
+            </Typography>
+            <Typography>
+              <b>Submission image:</b>
+            </Typography>
+
+            <div
+              style={{
+                margin: "0.5rem auto",
+                width: "100%",
+                textAlign: "center",
+              }}
+            >
+              <Image
+                priority
+                loader={imageLoader}
+                src={item.img_url}
+                height={450}
+                width={450}
+                alt={`${studentName}'s submission image: ${item.event_name}`}
+              />
+            </div>
+          </Paper>
+        ))}
+    </Dialog>
+  );
+}
---
+++ events/pages/students.tsx
@@ -0,0 +1,127 @@
+import { useEffect, useState } from "react";
+import StudentViewDialog from "../components/student_view_dialog";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import StudentUtil from "../utils/StudentUtil";
+import TextField from "@mui/material/TextField";
+import Layout from "../containers/layout";
+
+const StudentPaper = ({ item }) => {
+  const [openDialog, setOpenDialog] = useState(false);
+  const toggleOpenDialog = () => setOpenDialog(!openDialog);
+
+  return (
+    <>
+      <Paper
+        elevation={3}
+        sx={{
+          padding: "1rem",
+          margin: "1.25rem 0.7rem",
+        }}
+      >
+        <Typography>
+          <b>Student name:</b> {item.full_name}
+        </Typography>
+        <Typography>
+          <b>Signature count:</b> {item.signature_count} / 8
+        </Typography>
+
+        <div style={{ textAlign: "right" }}>
+          <Button onClick={toggleOpenDialog} variant="contained">
+            View more
+          </Button>
+        </div>
+      </Paper>
+
+      {openDialog && (
+        <StudentViewDialog
+          toggleOpenDialog={toggleOpenDialog}
+          openDialog={openDialog}
+          studentId={item.id}
+          studentName={item.full_name}
+          signatureCount={item.signature_count}
+        />
+      )}
+    </>
+  );
+};
+export default function SignatureTab({}) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [students, setStudents] = useState([]);
+  const [searchedStudents, setSearchedStudent] = useState([]);
+  useEffect(() => {
+    setIsLoading(true);
+
+    StudentUtil.fetchAllStudents()
+      .then((r) => {
+        setStudents(r);
+        setSearchedStudent(r);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.error("Error getting students");
+        setIsLoading(false);
+      });
+  }, []);
+
+  const downloadCsv = async () => {
+    try {
+      const blob = await (await fetch(`${origin}/api/student/csv`)).blob();
+
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "student_signatures.csv";
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+
+      URL.revokeObjectURL(url);
+    } catch (err) {
+      console.error("Error downloading csv file");
+    }
+  };
+
+  const handleChange = ({ target }) => {
+    setSearchedStudent(students);
+
+    return setSearchedStudent((prev) =>
+      prev.filter(
+        (item) =>
+          item.full_name.toLowerCase().indexOf(target.value.toLowerCase()) > -1
+      )
+    );
+  };
+
+  if (isLoading)
+    return (
+      <Layout title="GLC Student View">
+        <h2 style={{ textAlign: "center" }}>Loading...</h2>
+      </Layout>
+    );
+
+  if (students.length === 0)
+    return (
+      <Layout title="GLC Student View">
+        <h2 style={{ textAlign: "center" }}>No students have submitted yet</h2>
+      </Layout>
+    );
+
+  return (
+    <Layout title="GLC Student View">
+      <div style={{ margin: "0.6rem" }}>
+        <TextField
+          label="Type your name"
+          variant="filled"
+          onChange={handleChange}
+          sx={{ width: "100%" }}
+        />
+      </div>
+
+      {searchedStudents.map((item, index) => (
+        <StudentPaper key={index} item={item} />
+      ))}
+    </Layout>
+  );
+}
