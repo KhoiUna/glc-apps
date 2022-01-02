66d576c7cf152f263798c90802d44fc3a3f1805f
KhoiNguyen <thorwaitson@gmail.com>
2022-01-02 10:54:17 -0600

    admin: update login api

--- admin/pages/api/login.ts
+++ admin/pages/api/login.ts
@@ -1,15 +1,4 @@
-import { Client } from "pg";
-import dotenv from "dotenv";
-dotenv.config();
-
-const client = new Client({
-  connectionString: process.env.DB_STRING,
-  ssl: {
-    require: true,
-    rejectUnauthorized: false,
-  },
-});
-client.connect().catch((err) => console.error("err"));
+import client from "../../db/connection";
 
 export default async function loginRoute(req, res) {
   const { rows: students } = await client.query("SELECT * from students;");
