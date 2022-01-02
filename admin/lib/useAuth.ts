3fa74c9fd664328dd173631e268df7c8ac6a8a57
KhoiNguyen <thorwaitson@gmail.com>
2022-01-02 14:48:05 -0600

    admin: update useAuth

--- admin/lib/useAuth.ts
+++ admin/lib/useAuth.ts
@@ -2,12 +2,13 @@ import { useEffect } from "react";
 import Router from "next/router";
 import useSWR from "swr";
 
-export default function useAuth() {
+export default function useAuth({ redirectIfFound = false } = {}) {
   const fetcher = (url: string) => fetch(url).then((res) => res.json());
   const { data: user } = useSWR("/api/user", fetcher);
 
   useEffect(() => {
     if (!user?.isAuthenticated) Router.push("/login");
+    if (user?.isAuthenticated && redirectIfFound) Router.push("/");
   }, [user]);
 
   return { user };
