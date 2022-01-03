38d674895056c9ab172e40200e2cd374f0c2c60b
KhoiNguyen <thorwaitson@gmail.com>
2022-01-02 18:24:54 -0600

    server: update getStudents.js

--- server/utils/getStudents.js
+++ server/utils/getStudents.js
@@ -1,9 +1,15 @@
+const { QueryTypes } = require("sequelize");
+const connection = require("../db/connection");
 const Students = require("../db/Students");
 
 module.exports = async () => {
   try {
-    const res = await Students.findAll({
-      order: ["full_name"],
+    const sql =
+      "SELECT students.id, full_name, CAST(COUNT(students.id) AS INT) as signature_count FROM students JOIN submissions ON students.id = student_id WHERE status = :status GROUP BY students.id, full_name, status ORDER BY full_name;";
+    const res = await connection.query(sql, {
+      replacements: { status: "approved" },
+      model: Students,
+      type: QueryTypes.SELECT,
     });
     const students = res.map((i) => i.dataValues);
     return students;
