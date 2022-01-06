1a61ddeebc703e646738875d73aeafb0786476a2
KhoiNguyen <thorwaitson@gmail.com>
2022-01-05 23:48:09 -0600

    server: update saveEvent.js

--- server/utils/saveEvent.js
+++ server/utils/saveEvent.js
@@ -2,19 +2,21 @@ const Events = require("../db/Events");
 const sequelize = require("../db/connection");
 const createDiscordEvent = require("../helpers/createDiscordEvent");
 const { eventSubmissionOrigin } = require("../config");
+const dateDifference = require("../helpers/dateDifference");
 
 module.exports = async ({ date }) => {
   try {
     const d = new Date(date);
+    const createdDate = new Date(
+      new Date(d.getFullYear(), d.getMonth(), d.getDate())
+    ).toUTCString();
 
     let event;
     await sequelize.transaction(async (t) => {
       const res = await Events.create(
         {
-          created_at: new Date(
-            new Date(d.getFullYear(), d.getMonth(), d.getDate())
-          ).toUTCString(),
-          status: "opened",
+          created_at: createdDate,
+          status: dateDifference(createdDate) > 0 ? "pending" : "opened",
         },
         { transaction: t }
       );
@@ -29,6 +31,7 @@ module.exports = async ({ date }) => {
     return event;
   } catch (err) {
     console.error("Error saving event");
+    console.error(err);
     return;
   }
 };
