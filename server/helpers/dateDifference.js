1727eedb18dda1c0506b504b3fb9884fdccf7e26
KhoiNguyen <thorwaitson@gmail.com>
2022-01-05 23:58:42 -0600

    server: update dateDifference.js helper

--- server/helpers/dateDifference.js
+++ server/helpers/dateDifference.js
@@ -2,9 +2,11 @@ module.exports = (date1, date2 = new Date()) => {
   const dt1 = new Date(date1);
   const dt2 = new Date(date2);
 
-  return Math.floor(
-    (Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) -
-      Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) /
-      (1000 * 60 * 60 * 24)
+  return Math.abs(
+    Math.floor(
+      (Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) -
+        Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) /
+        (1000 * 60 * 60 * 24)
+    )
   );
 };
