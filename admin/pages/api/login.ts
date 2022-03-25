ba95b849222acb8b9bf43576202def8937ebddc8
KhoiNguyen <thorwaitson@gmail.com>
2022-03-25 13:00:13 -0500

    admin: update login route

--- admin/pages/api/login.ts
+++ admin/pages/api/login.ts
@@ -7,29 +7,34 @@ import blockMethods from "../../lib/blockMethods";
 import { User } from "../../types/types";
 
 async function loginRoute(req: NextApiRequest, res: NextApiResponse<User>) {
-  const { method } = req;
+  try {
+    const { method } = req;
 
-  if (!blockMethods(["POST"], method, res)) return;
+    if (!blockMethods(["POST"], method, res)) return;
 
-  const { username, password } = req.body;
-  const { rows } = await client.query(
-    "SELECT username, password from users WHERE username = $1;",
-    [username]
-  );
+    const { username, password } = req.body;
+    const { rows } = await client.query(
+      "SELECT username, password from users WHERE username = $1;",
+      [username]
+    );
 
-  if (
-    rows.length === 0 ||
-    !(await PasswordHelper.comparePassword(password, rows[0].password))
-  ) {
-    const user = { isAuthenticated: false, username: "" };
-    return res.json({ user });
+    if (
+      rows.length === 0 ||
+      !(await PasswordHelper.comparePassword(password, rows[0].password))
+    ) {
+      const user = { isAuthenticated: false, username: "" };
+      return res.json({ user });
+    }
+
+    const user = { username: rows[0].username, isAuthenticated: true };
+    req.session.user = { user };
+    await req.session.save();
+
+    res.json({ user });
+  } catch (err) {
+    console.error("Error logging in");
+    console.error(err);
   }
-
-  const user = { username: rows[0].username, isAuthenticated: true };
-  req.session.user = { user };
-  await req.session.save();
-
-  res.json({ user });
 }
 
 export default withIronSessionApiRoute(loginRoute, sessionOptions);
