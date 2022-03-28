d43a6b51e85a5bbed2d9f36d5a14572ad3734be7
KhoiNguyen <thorwaitson@gmail.com>
2022-03-28 11:52:13 -0500

    server: fix script to populate img ids

--- server/db/scripts/populateImgId.js
+++ server/db/scripts/populateImgId.js
@@ -3,6 +3,7 @@
 require("dotenv").config();
 const Submissions = require("../Submissions");
 const ImageKit = require("imagekit");
+const { Op } = require("sequelize");
 
 const imagekit = new ImageKit({
   publicKey: process.env.IMGKIT_PUBLIC_KEY,
@@ -12,39 +13,82 @@ const imagekit = new ImageKit({
 
 const parseFileName = (imgURL) => imgURL.split("glc_upload/")[1];
 
-const findImgId = async (fileName) => {
+const findImgId = async (submissionId, fileName) => {
   try {
     const res = await imagekit.listFiles({
       searchQuery: `name="${fileName}"`,
     });
-    console.log(res.fileId);
+    const { fileId } = res[0];
 
-    return res.fileId;
+    return fileId;
   } catch (error) {
-    console.error("Error finding image ID");
-    console.log(error);
-    return;
+    console.error("Error finding image ID for submission ID:", submissionId);
+    return process.exit();
+  }
+};
+
+const populateImgId = async () => {
+  try {
+    const res = await Submissions.findAll({
+      attributes: ["id", "img_url"],
+      order: ["id"],
+      where: {
+        img_id: {
+          [Op.eq]: null,
+        },
+      },
+    });
+
+    const fileNames = res.map((i) => ({
+      id: i.dataValues.id,
+      imageName: parseFileName(i.dataValues.img_url),
+    }));
+    console.log("fileNames length:", fileNames.length);
+    if (fileNames.length === 0) {
+      console.log("Done populating!");
+      return process.exit();
+    }
+
+    console.log("Last fileNames ID:", fileNames[fileNames.length - 1].id);
+    console.log();
+
+    const fileIDs = fileNames
+      .slice(
+        0,
+        fileNames.length === 1
+          ? fileNames.length
+          : Math.round(fileNames.length * 0.4, 0)
+      )
+      .map(async (i) => ({
+        id: i.id,
+        imageId: await findImgId(i.id, i.imageName),
+      }));
+
+    Promise.all(fileIDs)
+      .then((r) => {
+        r.forEach(async ({ id, imageId }) => {
+          if (imageId) {
+            const res = await Submissions.update(
+              {
+                img_id: imageId,
+              },
+              {
+                where: { id },
+              }
+            );
+            if (res) console.log("Updated", id);
+          }
+        });
+      })
+      .catch((e) => console.error(e));
+  } catch (err) {
+    console.error("Error populating image IDs");
+    return process.exit();
   }
 };
 
 (async () => {
-  try {
-    const res = await Submissions.findAll({
-      attributes: ["img_url"],
-    });
-    const fileNames = res.map((i) => parseFileName(i.dataValues.img_url));
-    const fileIDs = [];
-    fileNames.forEach((name) => {
-      setTimeout(() => {
-        findImgId(name).then((r) => console.log(r));
-      }, 1000);
-    });
-    // console.log(fileIDs);
-
-    return process.exit();
-  } catch (err) {
-    console.error("Error populating image IDs");
-    console.log(err);
-    return process.exit();
-  }
+  setInterval(() => {
+    populateImgId();
+  }, 10000);
 })();
