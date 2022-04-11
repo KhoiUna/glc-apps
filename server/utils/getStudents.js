84955473542a0486b1f8c8ba8d345ae5a51838ed
KhoiNguyen <thorwaitson@gmail.com>
2022-04-11 11:33:28 -0500

    server: update getStudents.js

--- server/utils/getStudents.js
+++ server/utils/getStudents.js
@@ -5,7 +5,7 @@ const Students = require("../db/Students");
 module.exports = async () => {
   try {
     const sql =
-      "SELECT students.id, full_name, CAST(COUNT(CASE WHEN submissions.status = 'approved' THEN 1 ELSE NULL END) AS INT) AS signature_count FROM students JOIN submissions ON students.id = student_id WHERE (status = 'approved' OR status='pending') GROUP BY students.id, full_name, status ORDER BY full_name;";
+      "SELECT students.id, full_name, CAST(COUNT(CASE WHEN submissions.status = 'approved' THEN 1 ELSE NULL END) AS INT) AS signature_count FROM students JOIN submissions ON students.id = student_id WHERE (status = 'approved' OR status='pending') GROUP BY students.id, full_name ORDER BY full_name;";
     const res = await connection.query(sql, {
       model: Students,
       type: QueryTypes.SELECT,
