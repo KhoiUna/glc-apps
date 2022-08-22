38b431250a372a64d025a784fceec1d9249395b4
KhoiNguyen <thorwaitson@gmail.com>
2022-08-22 19:44:19 +0700

    cooking: update Footer.js

--- cooking/components/Footer/Footer.js
+++ cooking/components/Footer/Footer.js
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer>
-      <p>&copy; {new Date().getFullYear()} Made with 💖</p>
+    <footer style={{ fontWeight: "bold" }}>
+      &copy; {new Date().getFullYear()} A product of{" "}
+      <Link href={"https://www.khoiuna.info"} passHref>
+        <a target={"_blank"} style={{ color: "#000" }}>
+          Khoi Nguyen
+        </a>
+      </Link>
     </footer>
   );
 }
